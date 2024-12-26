d5b10e066ea7b48af9ecc69d354d3acd79518cdf
Evan Nie <69750657+NieEvan@users.noreply.github.com>
2024-12-26 13:07:47 +0800

    Add files via upload

---
+++ StockScreener/main_us.py
@@ -0,0 +1,71 @@
+import os.path
+
+import pandas as pd
+from loguru import logger
+import MetaTrader5
+
+from base import Base
+from config import STKConfig
+
+
+class StrategyManage(Base):
+    def __init__(self) -> None:
+        # logger.add('logs\\stk.log')
+        STKConfig.pop("extra")
+        super().__init__("STK", None, ".", **STKConfig)
+        self.get_symbols()
+
+        self.first_run = False
+        self.log_run = True
+
+    def get_symbols(self) -> None:
+        # 通达信目录
+        tdx_path = r"C:\TDX"
+
+        mt5_symbols = {
+            symbol.name.replace('.NAS', ""): symbol.name for symbol in MetaTrader5.symbols_get(f"*.NAS")
+        }
+        mt5_symbols2 = {
+            symbol.name.replace('.NYSE', ""): symbol.name for symbol in MetaTrader5.symbols_get(f"*.NYSE")
+        }
+        mt5_symbols.update(mt5_symbols2)
+
+        # 筛选
+        # df = pd.read_csv("resource/us_stocks.csv")
+        # df = df[df["总金额"] >= 5000]
+        # df.sort_values("活跃度", ascending=False, inplace=True) 
+        # tdx_symbols = df["代码"].tolist()[:1000]TDX
+
+        # 读取通达信 USB USS
+        buy_path = os.path.join(tdx_path, r"T0002\blocknew\US3B.blk")
+        with open(buy_path, 'r') as f:
+            buy_blk = f.read()
+        sell_path = os.path.join(tdx_path, r"T0002\blocknew\US3S.blk")
+        with open(sell_path, 'r') as f:
+            sell_blk = f.read()
+        tdx_buy_symbols = [each.split("#")[-1] for each in buy_blk.split("\n") if each]
+        tdx_sell_symbols = [each.split("#")[-1] for each in sell_blk.split("\n") if each]
+
+        buy_symbols = []
+        for symbol in tdx_buy_symbols:
+            if symbol in mt5_symbols.keys():
+                buy_symbols.append(mt5_symbols[symbol])
+
+        sell_symbols = []
+        for symbol in tdx_sell_symbols:
+            if symbol in mt5_symbols.keys():
+                sell_symbols.append(mt5_symbols[symbol])
+
+        self.buy_symbols = buy_symbols
+        self.sell_symbols = sell_symbols
+        logger.info(f"len(self.buy_symbols): {len(self.buy_symbols)}")
+        logger.info(f"len(self.sell_symbols): {len(self.sell_symbols)}")
+
+
+def main() -> None:
+    sm = StrategyManage()
+    sm.start()
+
+
+if __name__ == '__main__':
+    main()
---
+++ StockScreener/strategies/SS_15M_S.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.append('F:/BaiduNetdiskDownload/FdtWarningV2/program/strategies/MyTT/')
+from MyTT import *
+
+
+class S15:
+    # 策略名称
+    def __init__(self):
+        # 策略周期
+        self.period = "15M"
+        # 策略方向
+        self.direction = "卖出"
+        self.conditions = f"{self.__class__.__name__}_{self.period}"
+
+    def on_signal(self, data):
+        # K线
+        KLINE = data[self.period][:-1]
+        OPEN, HIGH, LOW, CLOSE = KLINE['open'], KLINE['high'], KLINE['low'], KLINE['close']
+
+        # 指标
+        DIFF=EMA(CLOSE,12)-EMA(CLOSE,26)
+        DEA=EMA(DIFF,9)
+        MACD=(DIFF-DEA)*2
+        MA20=MA(CLOSE,20)
+        
+        
+        平台整理=( REF(HHV(CLOSE,3),1) - REF(LLV(OPEN,3),1)) / REF(LLV(OPEN,3),1)<=(3/100)
+        AB1=BARSLAST ( REF(CROSS(DEA,DIFF),1))
+        BB1=( REF(CLOSE,(AB1)+1)>CLOSE ) & ( REF(DIFF,(AB1)+1)>DIFF)
+        #AB2=BARSLAST ( REF(CROSS(DEA,DIFF),2))
+        #BB2=( REF(CLOSE,(AB2)+1) < REF(CLOSE,AB1+1) ) & ( REF(DIFF,(AB2)+1)>REF(DIFF,(AB1)+1))
+        
+        XG=( CROSS(MA20,CLOSE) ) & ( BB1 ) & ( DEA<3 )
+        RETURN=XG
+        return RETURN[-1]
+
